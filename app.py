87ee4b09e1d40e2a4953d5ad325065f4a3121b7a
Mikhail Timofeev <descentcare@gmail.com>
2022-08-23 00:49:14 +0500

    First normal version

--- app.py
+++ app.py
@@ -20,71 +20,61 @@ markup0 = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 markup0.add(*(KeyboardButton(x) for x in menu0))
 
 menu1 = (
-        "Корпусa",
+        "Корпуса",
         "Беседки 0 день",
         "В главное меню",
         )
 markup1 = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 markup1.add(*(KeyboardButton(x) for x in menu1))
 
-@bot.message_handler(commands=["start"])
-def start_message(message):
-    bot.send_message(message.chat.id, "Привет!", reply_markup=markup)
-
-@bot.message_handler(commands=["Программа"])
+@bot.message_handler(func = lambda message: message.text == "Программа")
 def program_message(message):
     bot.send_message( message.chat.id,
             "Программа на сегодняший вечер:",
             reply_markup = markup0)
 
-@bot.message_handler(commands=["Схема расположения"])
+@bot.message_handler(func = lambda message: message.text == "Схема расположения")
 def schemes_message(message):
     bot.send_message(message.chat.id,
             "Выбор схем расположения",
             reply_markup = markup1)
 
-@bot.message_handler(commands=["Корпуса"])
+@bot.message_handler(func = lambda message: message.text == "Корпуса")
 def blocks_message(message):
-    bot.send_message(message.chat.id,
-            "Корпуса",
+    bot.send_photo(message.chat.id, open('img/rd.jpg', 'rb'), 
             reply_markup = markup0)
-    bot.send_photo(message.chat.id, open('img/rb.jpg'))
 
-@bot.message_handler(commands=["Беседки 0 день"])
+@bot.message_handler(func = lambda message: message.text == "Беседки 0 день")
 def gazebos_message(message):
-    bot.send_message(message.chat.id,
-            "Беседки 0 день",
+    bot.send_photo(message.chat.id, open('img/rdg.jpg', 'rb'), 
             reply_markup = markup0)
-    bot.send_photo(message.chat.id, open('img/rbg.jpg'))
 
-@bot.message_handler(commands=["В главное меню"])
-def main_menu_message(message):
-    all_messages(message)
-
-@bot.message_handler(commands=["Транспорт + контакт"])
+@bot.message_handler(func = lambda message: message.text == "Транспорт + контакт")
 def contacts_message(message):
     bot.send_message(message.chat.id,
             "Ваши Транспорт + контакт",
             reply_markup = markup0)
 
-@bot.message_handler(commands=["Партнеры"])
+@bot.message_handler(func = lambda message: message.text == "Партнеры")
 def partners_message(message):
     bot.send_message(message.chat.id,
             "Наши Партнеры",
             reply_markup = markup0)
 
-@bot.message_handler(commands=["Обратная связь"])
+@bot.message_handler(func = lambda message: message.text == "Обратная связь")
 def partners_message(message):
-    bot.send_message( message.chat.id,
+    bot.send_message(message.chat.id,
             "Способы до нас достучаться:",
             reply_markup = markup0)
 
-@bot.message_handler(commands=["Covid"])
+@bot.message_handler(func = lambda message: message.text == "Covid")
 def partners_message(message):
-    bot.send_message( message.chat.id,
+    bot.send_message(message.chat.id,
             "Информация по коронавирусу: ",
             reply_markup = markup0)
 
+@bot.message_handler(commands=["start"])
+@bot.message_handler(func = lambda message: message.text == "В главное меню")
 @bot.message_handler(func=lambda message: message.text not in menu0+menu1)
 def all_messages(message):
     bot.send_message(message.chat.id, "Что ты хочешь узнать?", reply_markup=markup0)
